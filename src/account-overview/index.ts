cceb8e3b25b8f8fbb151d5cb557257d0578301a5
Ricky Huang <Ricky_Y_Huang@manulife.com>
2019-11-06 17:44:20 +0800

    scheme second

--- src/account-overview/index.ts
+++ src/account-overview/index.ts
@@ -17,7 +17,7 @@ import {strings} from '@angular-devkit/core';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function testone(options: any): Rule {
+export function main(options: any): Rule {
     return (tree: Tree, context: SchematicContext) => {
         if (!options.name) {
             throw new SchematicsException('Option (name) is required.');
